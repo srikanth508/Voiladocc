0f61055ab73184a9cbd075b04b73c999feb438ff
srikanth508 <srikanth@amazeinc.in>
2022-03-12 13:35:24 +0530

    config file

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,7 +5,7 @@ export default {
      SAMPLE_SERVER_BASE_URL:'https://voilodocopentokbox.herokuapp.com',
     //  Sessionurl:'https://madagascar.voiladoc.org/MarocAPI/Doctor/genratevideosessionID',
     //  Sessionurl:'https://madagascar.voiladoc.org/MarocAPI/Doctor/genratevideosessionID',
-    Sessionurl:'https://madagascar.voiladoc.org/MarocAPI/Doctor/genratevideosessionID',
+    Sessionurl:'https://maroc.voiladoc.org/MarocAPI/Doctor/genratevideosessionID',
 
    // Sessionurl:'https://14.192.17.225/MarocAPI/Doctor/genratevideosessionID',
 
